9ae0ce5e8c9a2c5de6ed8fb77a81130018783267
kaiyu li <krisfk@gmail.com>
2022-03-24 19:30:09 +0800

    .

--- wp-content/themes/astra-child/footer.php
+++ wp-content/themes/astra-child/footer.php
@@ -39,7 +39,7 @@ $title = $menu_item['title'];
 $class = $menu_item['class'];
 $temp_arr=explode(get_site_url(),$url);
 // echo 111233;
-////
+//////
 $slug=str_replace('/en/','',$temp_arr[1]);
 $slug=str_replace('/cn/','',$slug);
 $slug=str_replace('/','',$slug);
